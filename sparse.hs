8116e16826bb203bd013b1ddc6fa6b605237dbc9
Hans Höglund <hans@hanshoglund.se>
2013-08-23 21:37:10 +0200

    Cleaning

--- sparse.hs
+++ sparse.hs
@@ -22,50 +22,54 @@ instance Semigroup (Partial a b) where (<>) = mplus
 
 
 
-newtype PartialP a b = PartialP { getPartialP :: a -> Maybe (a, b) }
+newtype a ?-> b = PartialP { getPartialP :: a -> Maybe (a, b) }
     
-instance Functor (PartialP r) where
+instance Functor ((?->) r) where
     fmap f (PartialP g) = PartialP (fmap (fmap f) . g)
 
-instance Monad (PartialP r) where
+instance Monad ((?->) r) where
     return x = PartialP (\a -> Just (a, x))
     PartialP f >>= k = PartialP $ \r -> (f r >>= \(r1, x) -> getPartialP (k x) r1)
 
-instance MonadPlus (PartialP r) where
+instance MonadPlus ((?->) r) where
     mzero = PartialP (const Nothing)
     PartialP f `mplus` PartialP g = PartialP $ \x -> f x `mplus` g x
 
-instance Applicative (PartialP r) where
+instance Applicative ((?->) r) where
     pure  = return
     (<*>) = ap
 
-instance Alternative (PartialP r) where
+instance Alternative ((?->) r) where
     empty = mzero
     (<|>) = mplus
 
-instance Semigroup (PartialP a b) where
+instance Semigroup ((?->)    a b) where
     (<>) = mplus
 
-instance Monoid (PartialP a b) where
+instance Monoid ((?->) a b) where
     mempty  = mzero
     mappend = mplus
 
 
 
 
-newtype SparseT a b = SparseT { getSparseT :: PartialP a b }
-    deriving (Semigroup, Monoid, Functor, Applicative, Alternative, Monad, MonadPlus)
+-- newtype SparseT a b = SparseT { getSparseT :: a ?-> b }
+type SparseT = (?->)
+    -- deriving (Semigroup, Monoid, Functor, Applicative, Alternative, Monad, MonadPlus)
 
 type Sparse = SparseT String
 
 runSparseT :: SparseT a b -> a -> Maybe b
-runSparseT = fmap (fmap snd) . getPartialP . getSparseT
+runSparseT = fmap (fmap snd) . getPartialP
 
 runSparseT' :: SparseT a b -> a -> Maybe (a, b)
-runSparseT' = getPartialP . getSparseT
+runSparseT' = getPartialP
 
 runSparse :: Sparse a -> String -> Maybe a
 runSparse = runSparseT
+
+----------
+
 
 headP :: (a -> Bool) -> [a] -> Maybe ([a], a)
 headP p []     = Nothing
@@ -75,12 +79,20 @@ splitN :: ([a] -> Int) -> [a] -> Maybe ([a], [a])
 splitN p [] = Nothing
 splitN p ys = let n = p ys in if n < 1 then Nothing else Just (drop n ys, take n ys)
 
+----------
 
 char :: Char -> Sparse Char
-char c = SparseT $ PartialP $ headP (== c)
+char c = charIs (== c)
 
+charIs :: (Char -> Bool) -> Sparse Char
+charIs p = PartialP $ headP p
 
+string :: String -> Sparse String
+string s = stringIs (length s) (== s)
 
+stringIs :: Int -> (String -> Bool) -> Sparse String
+stringIs n p = PartialP $ splitN (\xs -> if p (take n xs) then n else 0)
+----------
 
 
 
